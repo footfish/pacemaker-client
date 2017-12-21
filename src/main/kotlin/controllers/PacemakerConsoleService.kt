48bed1c10e63f240ded597e3cf49d1e0f3e1dc47
Kevin Brennan <kevin.brennan@redsquared.com>
2017-12-21 17:55:44 +0000

    removed dependancy on Optional guava and added consoleUser object for logins

--- src/main/kotlin/controllers/PacemakerConsoleService.kt
+++ src/main/kotlin/controllers/PacemakerConsoleService.kt
@@ -1,5 +1,4 @@
 package controllers
-import com.google.common.base.Optional
 import asg.cliche.Command
 import asg.cliche.Param
 import java.util.ArrayList
@@ -14,10 +13,17 @@ import parsers.Parser
 class PacemakerConsoleService {
   private val paceApi = PacemakerAPI("http://localhost:7000")
   private val console = AsciiTableParser()
-	private var loggedInUser:User? = null
 	
-	 fun getLoggedInUser():String {
-		val result: String? = loggedInUser?.email 	 
+private	object consoleUser {
+		var id: String? = null
+	  fun loggedIn(): Boolean { if (id == null) return false else return true }
+	}
+
+//	private var loggedInUser:User? = null //to be removed 
+	
+/*	 fun getLoggedInUser():String {  // move to consoleUser
+		//val result: String? = loggedInUser?.email
+		val result: String? = consoleUser.id
 
 		if (result  != null)
     {
@@ -28,6 +34,8 @@ class PacemakerConsoleService {
       return "$"
     }
   }
+*/
+	
 	
   @Command(description = "Register: Create an account for a new user")
   fun register(@Param(name = "first name") firstName:String,
@@ -44,13 +52,13 @@ class PacemakerConsoleService {
 	@Command(description = "Login: Log in a registered user in to pacemaker")
   fun login(@Param(name = "email") email:String,
             @Param(name = "password") password:String) {
-    val user = Optional.fromNullable(paceApi.getUserByEmail(email))
-    if (user.isPresent())
+    val user = paceApi.getUserByEmail(email)
+    if (user != null)
     {
-      if (user.get().password.equals(password))
+      if (user.password.equals(password))
       {
-        loggedInUser = user.get() 
-        console.println("Logged in " + getLoggedInUser())
+        consoleUser.id = user.id 
+        console.println("Logged in " + user.email)
         console.println("ok")
       }
       else
@@ -59,42 +67,37 @@ class PacemakerConsoleService {
       }
     }
   }
-
+	
 	@Command(description = "Logout: Logout current user")
   fun logout() {
-    val user = Optional.fromNullable(loggedInUser)
-    if (user.isPresent())
+   if (consoleUser.loggedIn())
     {
-      console.println("Logging out " + getLoggedInUser())
-      console.println("ok")
-      loggedInUser = null
-    }
-    else
-    {
-      console.println("Sorry, you must be logged in to log out :)")
-    }
-  }
-	
+ 		println( "Logged out ")
+		consoleUser.id = null
+	  } else {
+ 		println("Nobody logged in")	   
+		}  
+	}
+			
   @Command(description = "Add activity: create and add an activity for the logged in user")
   fun addActivity(@Param(name = "type") type:String,
                   @Param(name = "location") location:String, @Param(name = "distance") distance:Float) {
-    val user = Optional.fromNullable(loggedInUser)
-    if (user.isPresent())
+   if (consoleUser.loggedIn())
     {
-      console.renderActivity(paceApi.createActivity(user.get().id, type, location, distance))
+      console.renderActivity(paceApi.createActivity(consoleUser.id!!, type, location, distance))
     }
     else
     {
       console.println("Not permitted, log in please!")
     }
   }
-	
+
+		
   @Command(description = "List Activities: List all activities for logged in user")
   fun listActivities() {
-    val user = Optional.fromNullable(loggedInUser)
-    if (user.isPresent())
+   if (consoleUser.loggedIn())
     {
-      console.renderActivities(paceApi.getActivities(user.get().id))
+      console.renderActivities(paceApi.getActivities(consoleUser.id!!))
     }
     else
     {
@@ -105,13 +108,12 @@ class PacemakerConsoleService {
   @Command(description = "Add location: Append location to an activity")
   fun addLocation(@Param(name = "activity-id") id:String,
                   @Param(name = "longitude") longitude:Double, @Param(name = "latitude") latitude:Double) {
-    val user = Optional.fromNullable(loggedInUser)
-    if (user.isPresent())
+   if (consoleUser.loggedIn())
     {
-      val activity = Optional.fromNullable(paceApi.getActivity(user.get().id, id))
-      if (activity.isPresent())
+      val activity = paceApi.getActivity(consoleUser.id!!, id)
+      if (activity !=  null)
       {
-        paceApi.addLocation(user.get().id, activity.get().id, latitude, longitude)
+        paceApi.addLocation(consoleUser.id!!, activity.id, latitude, longitude)
         console.println("ok")
       }
       else
@@ -127,10 +129,9 @@ class PacemakerConsoleService {
 	
   @Command(description = "ActivityReport: List all activities for logged in user, sorted alphabetically by type")
   fun activityReport() {
-    val user = Optional.fromNullable(loggedInUser)
-    if (user.isPresent())
+   if (consoleUser.loggedIn())
     {
-      console.renderActivities(paceApi.getActivities(user.get().id))
+      console.renderActivities(paceApi.getActivities(consoleUser.id!!))
     }
     else
     {
@@ -140,8 +141,7 @@ class PacemakerConsoleService {
 	
   @Command(description = "Activity Report: List all activities for logged in user by type. Sorted longest to shortest distance")
   fun activityReport(@Param(name = "byType: type") type:String) {
-    val user = Optional.fromNullable(loggedInUser)
-    if (user.isPresent())
+   if (consoleUser.loggedIn())
     {
     /*  val reportActivities = ArrayList<E>()
       val usersActivities = paceApi.getActivities(user.get().getId())
@@ -161,11 +161,10 @@ class PacemakerConsoleService {
 	
  @Command(description = "List all locations for a specific activity")
  fun listActivityLocations(@Param(name = "activity-id") id:String) {
-   val user = Optional.fromNullable(loggedInUser)
-   if (user.isPresent())
+   if (consoleUser.loggedIn())
      {
-     val activity = Optional.fromNullable(paceApi.getActivity(user.get().id, id))
-     if (activity.isPresent())
+     val activity = paceApi.getActivity(consoleUser.id!!, id)
+     if (activity != null)
        {
         // 	console.renderLocations(activity.get().route);
        }
