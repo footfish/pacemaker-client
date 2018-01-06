69c46974cf645c63ffea570da4447e30d415d29f
Kevin Brennan <kevin.brennan@redsquared.com>
2018-01-06 23:53:07 +0000

    remove named object in ClientMain or jar can't find main()

--- src/main/kotlin/controllers/ClientMain.kt
+++ src/main/kotlin/controllers/ClientMain.kt
@@ -6,11 +6,9 @@ import asg.cliche.ShellFactory
 import asg.cliche.ShellDependent
 import java.io.IOException
 
-object ClientMain{
 		val console = PacemakerConsoleService("http://localhost:7000")
 		
     @Throws(IOException::class)
-    @JvmStatic fun main(args:Array<String>) {
+     fun main(args:Array<String>) {
       ShellFactory.createConsoleShell( "$", "Welcome to pacemaker-console - ?help for instructions", console).commandLoop()
     } 
-}
